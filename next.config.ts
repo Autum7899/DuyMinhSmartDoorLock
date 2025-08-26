35e41442e805f02d80c294dae5d62f555ab683ba
Autum7899 <minhson78999@gmail.com>
2025-08-26 18:15:51 +0700

    image

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'pub-1ad0b58a12834136a469dfd3b6644387.r2.dev',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
   /* config options here */
 };
 
